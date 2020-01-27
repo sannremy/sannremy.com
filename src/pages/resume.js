b03c118615243767dfdd03ab4e5cdb2682a9c582
Sann-Remy Chea <sannremy.chea@gmail.com>
2020-01-27 18:09:24 +0100

    Saved

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,67 +1,135 @@
 import React from "react"
 import Layout from "../components/layout"
+import tennisEmoji from "../assets/openmoji-svg-color/1F3BE.svg"
+import catLoveEmoji from "../assets/openmoji-svg-color/1F63B.svg"
 
 const Resume = () => (
   <Layout>
-    <div className="center f6">
+    <div className="mb5">
       <h1 className="ma0">Resume</h1>
       <p className="lh-copy">
         I am blabla
       </p>
     </div>
 
-    <h1 className="f4 mb3 mt0 lh-title">Experience</h1>
+    <div className="mb3">
+      <h2 className="f3 mb3 mt0 lh-title">Work Experience</h2>
 
-    <header className="fn fl-ns w-50-ns pr4-ns dark-gray">
-      <time className="f7 ttu tracked gray">Jan Tschichold</time>
-    </header>
-    <div className="fn fl-ns w-50-ns dark-gray">
-      <p className="lh-copy mt2 mt0-ns">
-        PERFECT typography is more a science than an art. Mastery of the trade is
-        indispensable, but it isn't everything.
-      </p>
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <p className="lh-copy ma0">
+            Blizzard
+          </p>
+        </div>
+        <div className="fl w-70 v-top">
+          <p className="lh-copy ma0">
+            PERFECT typography is more a science than an art. Mastery of the trade is
+            indispensable, but it isn't everything.
+          </p>
+        </div>
+      </section>
+
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <p className="lh-copy ma0">
+            Blizzard
+          </p>
+        </div>
+        <div className="fl w-70 v-top">
+          <p className="lh-copy ma0">
+            PERFECT typography is more a science than an art. Mastery of the trade is
+            indispensable, but it isn't everything.
+          </p>
+        </div>
+      </section>
     </div>
 
-    <header className="fn fl-ns w-50-ns pr4-ns dark-gray">
-      <time className="f7 ttu tracked gray">Jan Tschichold</time>
-    </header>
-    <div className="fn fl-ns w-50-ns dark-gray">
-      <p className="lh-copy mt2 mt0-ns">
-        PERFECT typography is more a science than an art. Mastery of the trade is
-        indispensable, but it isn't everything.
-      </p>
+    <div className="mb3">
+      <h2 className="f3 mb3 mt0 lh-title">Education</h2>
+
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <p className="lh-copy ma0">
+            EISTI
+          </p>
+        </div>
+        <div className="fl w-70 v-top">
+          <p className="lh-copy ma0">
+            PERFECT typography is more a science than an art. Mastery of the trade is
+            indispensable, but it isn't everything.
+          </p>
+        </div>
+      </section>
+
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <p className="lh-copy ma0">
+            EISTI
+          </p>
+        </div>
+        <div className="fl w-70 v-top">
+          <p className="lh-copy ma0">
+            PERFECT typography is more a science than an art. Mastery of the trade is
+            indispensable, but it isn't everything.
+          </p>
+        </div>
+      </section>
     </div>
 
-    <div className="lh-copy center f6 dark-gray">
-      <h4 className="f4 fw6">Skills</h4>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">Programming languages</dt>
-        <dd className="dib ml0 gray">JavaScript, Go, PHP</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">L</dt>
-        <dd className="dib ml0 gray">Losses</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">PCT</dt>
-        <dd className="dib ml0 gray">Winning Percentages</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">GB</dt>
-        <dd className="dib ml0 gray">Games Back</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">Home</dt>
-        <dd className="dib ml0 gray">Home Record</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">Road</dt>
-        <dd className="dib ml0 gray">Road Record</dd>
-      </dl>
-      <dl className="f6 lh-title mv2">
-        <dt className="dib b pr1">Div</dt>
-        <dd className="dib ml0 gray">Division Record</dd>
-      </dl>
+    <div className="mb3">
+      <h2 className="f3 mb3 mt0 lh-title">Skills</h2>
+
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <span className="lh-copy">Programming languages</span>
+        </div>
+        <div className="fl w-70 v-top">
+          <ul className="list pa0 ma0 center">
+            <li className="lh-copy">Orange</li>
+            <li className="lh-copy">Apple</li>
+            <li className="lh-copy">Peach</li>
+            <li className="lh-copy">Grape</li>
+            <li className="lh-copy">Grapefruit</li>
+            <li className="lh-copy">Kiwi</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="cf">
+        <div className="fl w-30 v-top pr3">
+          <span className="lh-copy">Programming</span>
+        </div>
+        <div className="fl w-70 v-top">
+          <ul className="list pa0 ma0 center">
+            <li className="lh-copy">Orange</li>
+            <li className="lh-copy">Apple</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+
+    <div className="mb3">
+      <h2 className="f3 mb3 mt0 lh-title">Interests</h2>
+
+      <section className="cf">
+        <ul className="list pa0 ma0 center">
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Tennis<img className="dib w1 v-mid pl1" src={tennisEmoji} /></span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Cats<img className="dib w1 v-mid pl1" src={catLoveEmoji} /></span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Gaming 1F3AE</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Nerd 1F913</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Raspberry Pi E1C9</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Code E1C1</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Dad jokes 1F468-200D-1F469-200D-1F467-200D-1F466</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Bengal</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Birman</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Bombay</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Brazilian</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">British</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">British</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">British</span></li>
+          <li className="dib mr1 mb2"><span className="db pa2 dark-gray ba b--black-10">Burmese</span></li>
+        </ul>
+      </section>
     </div>
   </Layout>
 )
