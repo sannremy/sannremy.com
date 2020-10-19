10f6adcd5bee0c36b2471543e3c0cf8f8621cb8e
Sann-Remy Chea <sannremy.chea@gmail.com>
2020-10-19 10:42:38 +0200

    Added favicons

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,12 @@ class Home extends React.Component {
           <meta name="title" content={title} />
           <meta name="description" content={description} />
 
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#ffffff" />
+
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://sannremy.com" />
           <meta property="og:title" content={title} />
