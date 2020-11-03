1af92d7a1a35256a02b21d658507ba8b16d0e84a
Sann-Remy Chea <sannremy.chea@gmail.com>
2020-11-03 11:53:33 +0100

    Switched buttons for UX

--- pages/index.js
+++ pages/index.js
@@ -102,18 +102,32 @@ export default function Home() {
       <main>
         <div className="flex items-center justify-center flex-col">
           {/* Action bar */}
-          <div className="flex items-center justify-end w-full mb-5">
-            <span onClick={() => setDarkMode(!isDarkMode)} className="block mr-1 flex items-center px-2 py-2 border border-gray-300 dark:border-gray-700 hover:border-gray-700 dark-hover:border-gray-500 cursor-pointer transition-colors duration-150 ease-in-out rounded-full">
-              <span className="flex-none block w-4">
-                {isDarkMode && (
+          <div className="flex items-center justify-center w-full mb-5">
+            <span onClick={() => setDarkMode(!isDarkMode)} className="mr-1 block text-sm flex items-center px-2 py-1 border border-gray-300 dark:border-gray-700 hover:border-gray-700 dark-hover:border-gray-500 cursor-pointer transition-colors duration-150 ease-in-out rounded-full">
+              <span className="flex-none block w-4 mr-1">
+                {!isDarkMode && (
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                   </svg>
                 )}
-                {!isDarkMode && (
+                {isDarkMode && (
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                   </svg>
+                )}
+              </span>
+              <span>
+                {!isDarkMode && (
+                  <FormattedMessage
+                    id="lightMode"
+                    defaultMessage=""
+                  />
+                )}
+                {isDarkMode && (
+                  <FormattedMessage
+                    id="darkMode"
+                    defaultMessage=""
+                  />
                 )}
               </span>
             </span>
