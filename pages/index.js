931f18ec237297e7dbd60e0c6cb184115b3ead11
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2021-03-22 16:52:16 +0100

    Fixed SVG FOUC

--- pages/index.js
+++ pages/index.js
@@ -90,14 +90,14 @@ export default function Home() {
           {/* Action bar */}
           <div className="grid grid-cols-2 gap-2 w-full mb-5">
             <span onClick={() => toggleDarkMode()} className="place-self-end block text-sm flex items-center px-2 py-1 opacity-75 bg-gray-200 dark:bg-gray-700 hover:opacity-100 rounded-full cursor-pointer transition-colors duration-150 ease-in-out">
-              <span className="flex-none block w-4 mr-1">
+              <span className="flex-none block w-4 h-4 mr-1">
                 {!isDarkMode && (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                   </svg>
                 )}
                 {isDarkMode && (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                   </svg>
                 )}
@@ -112,7 +112,7 @@ export default function Home() {
             >
               <span className="place-self-start block text-sm flex items-center px-2 py-1 opacity-75 bg-gray-200 dark:bg-gray-700 hover:opacity-100 rounded-full cursor-pointer transition-colors duration-150 ease-in-out">
                 <span className="flex-none block w-4 mr-1">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
                   </svg>
                 </span>
