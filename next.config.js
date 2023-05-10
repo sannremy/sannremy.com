aa6dbc996152b0fe73bdb3b54c918a3656f3f628
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-05-10 16:36:25 +0200

    Added umami

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
     umami: {
-      id: '11d4ba42-658e-4085-80be-1b1a3a86a9a8',
-      js: 'https://analytics.kangur.us/script.js',
-      domain: 'sannremy.com',
+      id: process.env.UMAMI_ID,
+      js: process.env.UMAMI_JS,
+      domain: process.env.UMAMI_DOMAIN,
     },
   },
 }
